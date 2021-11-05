e1d6c2addc56ff681103e0afdbb9eef93ea54177
G-Salis <giuseppesalislavoro@gmail.com>
2021-11-05 14:58:14 +0100

    Funzione per creazioni post

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+
+let container = document.getElementById('container');
+
 const posts = [
     {
         "id": 1,
@@ -55,3 +58,44 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+addPost();
+
+function addPost() {
+    for (let i in posts){
+        container.innerHTML += `
+
+          <div class="post">
+          <div class="post__header">
+              <div class="post-meta">                    
+                  <div class="post-meta__icon">
+                      <img class="profile-pic" src="${posts[i].author.image}" alt="Phil Mangione">                    
+                  </div>
+                  <div class="post-meta__data">
+                      <div class="post-meta__author">${posts[i].author.name}</div>
+                      <div class="post-meta__time">4 mesi fa</div>
+                  </div>                    
+              </div>
+          </div>
+          <div class="post__text">${posts[i].content}</div>
+          <div class="post__image">
+              <img src="${posts[i].media}" alt="">
+          </div>
+          <div class="post__footer">
+              <div class="likes js-likes">
+                  <div class="likes__cta">
+                      <a class="like-button  js-like-button" href="#" data-postid="1">
+                          <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                          <span class="like-button__label">Mi Piace</span>
+                      </a>
+                  </div>
+                  <div class="likes__counter">
+                      Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+                  </div>
+              </div> 
+          </div>            
+      </div>
+          `;
+      }
+    
+  }
